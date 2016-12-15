6a90f788bcb349f592fd62b3113aa4705abdbf72
Bmbul <smbat.eghiazaryan@gmail.com>
2016-12-15 13:24:38 +0300

    Update boxes.js

--- boxes.js
+++ boxes.js
@@ -1,11 +1,13 @@
-function Breaks() {
-  this.unit = 60;
+function Breaks(x,y) {
+ 
+  this.x = x;
+  this.y = y;
   
- /* this.createBreaks = function () {
-    for (var i = 0; i < width/this.unit; i++) {
-      for (var j = 0; j < height/this.unitt; j++) {
-        blocks[i][j] = rect( this.unit*(j+1), this.unit*(i+1), this.unit/2, this.unit/2 );
-        }
-    }
+  this.length = 70;
+  
+  this.height = 30;
+  
+  this.createBreaks = function () {
+	rect(this.x,this.y,this.length,this.height);
   }
-}*/
+} 
